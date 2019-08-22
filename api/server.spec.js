3fe85964eddc73b208a9701cec32ada68513622e
York Cook <yorkcook@Johnny-Castle.local>
2019-08-22 17:53:10 -0400

    WIP: test set up

---
+++ api/server.spec.js
@@ -0,0 +1,88 @@
+const request = require("supertest");
+
+const db = require("../data/dbConfig.js");
+
+const server = require("./server.js");
+
+describe("server", () => {
+  beforeEach(async () => {
+    await db("ogs").truncate();
+  });
+
+  describe("GET /", () => {
+    it("returns 200 OK", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it("returns JSON", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.type).toMatch(res.json);
+        });
+    });
+  });
+
+  describe("GET /ogs", () => {
+    it("should return an array", () => {
+      return request(server)
+        .get("/ogs")
+        .then(res => {
+          expect(Array.isArray(res.body)).toBe(true);
+        });
+    });
+  });
+
+  describe("POST /ogs", () => {
+    it("should insert an og into the db", () => {
+      return request(server)
+        .post("/ogs")
+        .send({
+          name: "Steve Jobs"
+        })
+        .then(res => {
+          expect(res.body.length).toBe(1);
+        });
+    });
+
+    it("should insert more than one OG", async () => {
+      await request(server)
+        .post("/ogs")
+        .send([
+          {
+            name: "Brad Pitt"
+          },
+          {
+            name: "Ron Paul"
+          },
+          {
+            name: "Derek Jeter"
+          }
+        ]);
+
+      const ogs = await db("ogs");
+      expect(ogs).toHaveLength(3);
+    });
+  });
+});
+
+describe("delete /ogs", () => {
+  it.skip("return deleted 200", () => {
+    return request(server)
+      .delete("/ogs/")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("return 2 after deleting", () => {
+    return request(server)
+      .delete("/ogs/1")
+      .then(res => {
+        expect(res.body).toBe(2);
+      });
+  });
+});
